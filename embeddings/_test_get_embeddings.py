d7b16d7bfc2caf2e7c54fc6d2064981475f1551e
maxwelllwang <maxwelllwang@gmail.com>
2024-05-03 00:42:18 -0700

    add: more detailed model outline architecture should be set

--- embeddings/_test_get_embeddings.py
+++ embeddings/_test_get_embeddings.py
@@ -1,6 +1,9 @@
 from get_embeddings import ICD9Embeddings
 
 embeddings = ICD9Embeddings()
+print(len(embeddings.get('5723')))
+print(len(embeddings.get('78959')))
+print(len(embeddings.get('5715')))
 print(len(embeddings.get('07070')))
 
-print(repr(embeddings.get('07070')))
+# print(repr(embeddings.get('5723')))
